5a653e41ac45cfa4d78795cbf191b7ec32adbcb5
Avaneesh-Pathak <avaneeshpathak900@gmail.com>
2026-07-05 18:50:31 +0530

    Updated

--- server.js
+++ server.js
@@ -43,15 +43,7 @@ const transporter = nodemailer.createTransport({
     }
 
 });
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-    }
-});
+
 transporter.verify((err)=>{
 
     if(err){
