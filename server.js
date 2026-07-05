92380ac1b3913c5348c20ec6d9ea74328530bb7f
Avaneesh-Pathak <avaneeshpathak900@gmail.com>
2026-07-05 18:24:57 +0530

    Email sender added

--- server.js
+++ server.js
@@ -43,7 +43,15 @@ const transporter = nodemailer.createTransport({
     }
 
 });
-
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+});
 transporter.verify((err)=>{
 
     if(err){
